e2766a4ac7deeecf9f00d972e3b62708f96070ce
Marco Antônio de Ulhôa Cintra <marcoantoniocintra@gmail.com>
2020-11-30 16:56:33 -0300

    Update main.py

--- cml/main.py
+++ cml/main.py
@@ -40,7 +40,8 @@ def setMap(mapping):
 	elif(mapping=='ts'):
 		cmlMap = maps.ts
 		#print("recebi o parâmetro do 'ts' certinho?:")
-		print(sys.argv)
+		num = sys.argv[4]*sys.argv[7]*5
+		print(num)
 		par = np.genfromtxt(sys.argv[-1], delimiter=',')
 		print("estou em main.py. li as linhas do 'ts' certinho?:")
 		print(par)
