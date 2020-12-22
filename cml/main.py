114d840ac381ffee5e94d0b367c7c64ecb5b7a08
Marco Antônio de Ulhôa Cintra <marcoantoniocintra@gmail.com>
2020-12-22 14:14:27 -0300

    Update main.py

--- cml/main.py
+++ cml/main.py
@@ -120,10 +120,10 @@ if __name__ == "__main__":
                         with open('output/csv/matrixfile.csv', 'a') as f:
                                 np.savetxt(f, c.mat)
                                 if(i!=nit-1):
-                                        print("dentro " + str(i))
-                                        f.write("\n")
+                                        print("\n dentro " + str(i))
+                                        #f.write("\n")
                                 else:
-                                        print("fora " + str(i))
+                                        print("\n fora " + str(i))
                 if ('-grad' in sys.argv):
                         mod, phase = c.getGradient(x,y)
                         grad.append([mod,phase])
