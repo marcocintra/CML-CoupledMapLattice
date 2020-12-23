f0e74d4f1e0524e8f08a5f8cc9d8ffc0e02652bb
Marco Antônio de Ulhôa Cintra <marcoantoniocintra@gmail.com>
2020-12-22 21:46:07 -0300

    Update maps.py

--- cml/maps.py
+++ cml/maps.py
@@ -8,14 +8,7 @@ def logisticMap(x, par):
     else:
         a = 4.0
     return (a * x * (1.0 - x))
-    
-def dlogisticMap(x, par):
-    if(len(par)>0):
-        a = par[0]
-    else:
-        a = 4.0
-    return (a -2.0*a*x) 
-    
+  
 def onebyfMap(u, beta):
     temp = cn.powerlaw_psd_gaussian(beta, (u.shape[0], u.shape[1]))
     temp = ((temp - np.min(temp))/np.ptp(temp)) * 0.1 + u
