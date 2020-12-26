b8f02c8f7046bbee9b14e0f71ba2db16c9f530fa
Marco Antônio de Ulhôa Cintra <marcoantoniocintra@gmail.com>
2020-12-26 16:37:40 -0300

    Update maps.py

--- cml/maps.py
+++ cml/maps.py
@@ -16,9 +16,9 @@ def logisticMap(x, par):
     return (a * x * (1.0 - x))
   
 def onebyfMap(x, beta, grid, snapcont):
-    print("!!!!!! "+str(grid[0]))
-    if(snapcont == 1):
-            temp = cn.powerlaw_psd_gaussian(beta, (2, 2))
+    #print("!!!!!! "+str(grid[0]))
+    #if(snapcont == 1):
+    temp = cn.powerlaw_psd_gaussian(beta, (2, 2))
             #temp = ((temp - np.min(temp))/np.ptp(temp)) * 0.1 + x
             #temp = (temp - np.min(temp))/np.ptp(temp)
             #print("temp...cnapcont==1... "+str(temp))
