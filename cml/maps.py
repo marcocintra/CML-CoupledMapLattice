80c45eab33f3e0dfc23bdd27d197b466da60db52
Marco Antônio de Ulhôa Cintra <marcoantoniocintra@gmail.com>
2020-12-26 21:23:00 -0300

    Update maps.py

--- cml/maps.py
+++ cml/maps.py
@@ -22,7 +22,10 @@ def onebyfMap(x, beta, grid, nit,snapshot):
             #print("\n")
             valores = (grid[0]**2)*(nit)*(5)
             newarr = np.empty((1))
-            for i in range(int(valores/grid[0]**2)):
+            matrizes = int(valores/grid[0]**2)
+            print("matrizes "+str(matrizes))
+            print("\n")
+            for i in range(matrizes):
                     temp = cn.powerlaw_psd_gaussian(beta, (grid[0], grid[0]))
                     temp = ((temp - np.min(temp))/np.ptp(temp)) * 0.1 + x
                     temp = (temp - np.min(temp))/np.ptp(temp)
