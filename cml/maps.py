c8430aee7bcc91e6712be0a279b388df52aa3048
Marco Antônio de Ulhôa Cintra <marcoantoniocintra@gmail.com>
2021-01-15 23:22:05 -0300

    Update maps.py

--- cml/maps.py
+++ cml/maps.py
@@ -17,8 +17,7 @@ def logisticMap(x, par):
 def work(beta, size):
     return np.array(cn.powerlaw_psd_gaussian(beta, size))
 
-
-def onebyfMap(x, beta, grid, nit,snapshot):
+def onebyfMap(x, beta, grid, nit, snapshot):
     global cont
     global noises
     global matrixnoises
@@ -27,7 +26,38 @@ def onebyfMap(x, beta, grid, nit,snapshot):
     valoresporit = int(valores/nit)
     beta = int(beta)
     if(cont == 0):
-            matrixnoises = np.array([]).reshape(0,valoresporit)
+        matrixnoises = np.array([]).reshape(0,valoresporit)
+
+        workers = multiprocessing.cpu_count()
+
+        with futures.ProcessPoolExecutor(workers) as executor:
+            to_do = []
+            for i in range(nit):
+                future = executor.submit(work, beta, valoresporit)
+                future.sid = 'noise snapshot {}'.format(i)
+                to_do.append(future)
+
+            for future in futures.as_completed(to_do):
+                # Para um array com os ruídos de todos os snapshots
+                res = future.result()
+                matrixnoises = np.vstack([matrixnoises, res])
+                print(f'#{future.sid} successfully generated!')
+
+    indice = cont-(valoresporit*(snapshot+1))
+    cont = cont + 1
+    return matrixnoises[snapshot][indice] + x
+
+
+#def onebyfMap(x, beta, grid, nit,snapshot):
+#    global cont
+#    global noises
+#    global matrixnoises
+#    valores = (grid[0]**2)*(nit)*(5)
+#    matrizes = int(valores/grid[0]**2)
+#    valoresporit = int(valores/nit)
+#    beta = int(beta)
+#    if(cont == 0):
+#            matrixnoises = np.array([]).reshape(0,valoresporit)
             #print("cont  "+str(cont))
             #print("\n")
             #print("valores  "+str(valores))
@@ -36,8 +66,8 @@ def onebyfMap(x, beta, grid, nit,snapshot):
             #print("\n")
             #print("valores por iteração "+str(valoresporit))
             #print("\n")
-            for i in range(nit):
-                    noises = cn.powerlaw_psd_gaussian(beta, valoresporit)
+ #           for i in range(nit):
+ #                   noises = cn.powerlaw_psd_gaussian(beta, valoresporit)
                     #noises = ((noises - np.min(noises))/np.ptp(noises)) * 0.1
                     #print("noises" + str(noises))
                     #print("\n")
@@ -45,7 +75,7 @@ def onebyfMap(x, beta, grid, nit,snapshot):
                     #print("\n")
                     #print("shape noises" + str(np.shape(noises)))
                     #print("\n")
-                    matrixnoises = np.vstack([matrixnoises,noises]) 
+ #                   matrixnoises = np.vstack([matrixnoises,noises]) 
                     #print("matrixnoises" + str(matrixnoises))
                     #print("\n")
                     #print("type matrixnoises" + str(type(matrixnoises)))
@@ -58,11 +88,11 @@ def onebyfMap(x, beta, grid, nit,snapshot):
     #print("\n")
     #print("nit "+str(nit))
     #print("\n")
-    indice = cont-(valoresporit*(snapshot+1))
-    cont = cont + 1
+    #indice = cont-(valoresporit*(snapshot+1))
+    #cont = cont + 1
     #print("\n")
     #print("parâmetro x: "+str(x))
     #print("\n")
     #print("grid: "+str(grid))
     #print("\n")        
-    return matrixnoises[snapshot][indice] + x
+    #return matrixnoises[snapshot][indice] + x
