b8facc3318a321c38b84d13a888ff0e23fce0c8b
Marco Antônio de Ulhôa Cintra <marcoantoniocintra@gmail.com>
2021-01-20 22:25:26 -0300

    Update CML.py

--- cml/CML.py
+++ cml/CML.py
@@ -83,7 +83,7 @@ class CML():
                 mat_shape = rows, cols = self.mat.shape
                 if(function.__name__ == 'onebyfMap'):
                         outputMat = (1.0-coupling) * function(self.mat, parameters, mat_shape, nit, snapshot)
-                        for ny, nx in neighbouhood:
+                        for ny, nx in neighborhood:
                                 outputMat += (coupling/len(neighborhood)) * function(self.mat[np.roll(np.arange(rows), -nx), np.roll(np.arange(cols), -ny)], parameters, mat_shape, nit, snapshot)
                 else:
                         outputMat[i][j] = (1.0-coupling) * function(self.mat[i][j],parameters)  
